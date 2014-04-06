1237b22ce3e7b98fcef700752a441ab91e59293b
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-04-06 16:30:01 -0300

    added more tests

--- src/main/java/com/contaazul/coverage/github/GitHubException.java
+++ src/main/java/com/contaazul/coverage/github/GitHubException.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 public class GitHubException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	public GitHubException(String msg) {
-		super( msg );
-	}
-
 	public GitHubException(String msg, IOException e) {
 		super( msg, e );
 	}
